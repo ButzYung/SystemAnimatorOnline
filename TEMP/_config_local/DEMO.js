dc9dea56c5135e9cef657562981ab5c22c13ff40
ButzYung <butz@animetheme.com>
2019-04-09 04:57:47 +0800

    delete temp

--- TEMP/_config_local/DEMO.js
+++
@@ -1,9 +0,0 @@
-/* Gadget local config (v1.2.0) */
-/* F:\\Programs Portable\\node-webkit\\AT_SystemAnimator_v0001.gadget\\TEMP\\DEMO\\dance.EQP-bg_bpm150-0_br0-0.mp4 */
-SystemEXT._default._settings = {
-"Folder":"$SA_HTA_folder$%5Cdance.EQP-bg_bpm150-0_br0-0.mp4",
-"_screenLeft":"573",
-"_screenTop":"47",
-"LABEL_Folder":"$SA_HTA_folder$%5Cdance.EQP-bg_bpm150-0_br0-0.mp4"
-};
-if (use_SA_browser_mode) { System.Gadget.Settings._settings = SystemEXT._default._settings; } else if (!System.Gadget.path) { System.Gadget.path = "F:\\Programs Portable\\node-webkit\\AT_SystemAnimator_v0001.gadget"; };
--- TEMP/_config_local/xpmce_tan.js
+++
@@ -1,8 +0,0 @@
-/* Gadget local config (v1.2.0) */
-SystemEXT._default._settings = {
-"WallpaperAsBG":"non_default",
-"_screenLeft":"640",
-"_screenTop":"308",
-"LABEL_Folder":"$SA_HTA_folder$"
-};
-if (use_SA_browser_mode) { System.Gadget.Settings._settings = SystemEXT._default._settings; } else if (!System.Gadget.path) { System.Gadget.path = "F:\\Programs Portable\\node-webkit\\AT_SystemAnimator_v0001.gadget"; };
