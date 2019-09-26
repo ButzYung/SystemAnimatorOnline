9c401fbef607be743f8b41339a0141683ae31c51
ButzYung <butz@animetheme.com>
2019-09-27 01:32:07 +0800

    Update MMD_SA.js

--- MMD.js/MMD_SA.js
+++ MMD.js/MMD_SA.js
@@ -4364,7 +4364,7 @@ xr.XR_objects_by_id = {}
 MMD_SA.scene.__objects.forEach(function (obj, idx) {
 // top-level objects/MMD models only
 // Note: MMD model(SkinnedMesh) is wrapped by Object3D. Ignore that wrapper.
-  if ((obj == xr.reticle) || ((obj.parent != MMD_SA.scene) && !(obj instanceof THREE.SkinnedMesh)) || (obj.children[0] instanceof THREE.SkinnedMesh)) return;
+  if (((obj.parent != MMD_SA.scene) && !(obj instanceof THREE.SkinnedMesh)) || (obj.children[0] instanceof THREE.SkinnedMesh)) return;
 //DEBUG_show(idx,0,1)
   if (!obj._XR_id)
     obj._XR_id = THREE.Math.generateUUID()
