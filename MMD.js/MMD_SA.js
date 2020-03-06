408b6507f815351690e47f6919c00fb9ea85b372
ButzYung <butz@animetheme.com>
2020-03-07 04:00:55 +0800

    Update MMD_SA.js

--- MMD.js/MMD_SA.js
+++ MMD.js/MMD_SA.js
@@ -4769,7 +4769,7 @@ else {
   if (touches.length == 2) {
 DEBUG_show(Date.now())
   }
-DEBUG_show(Date.now()+'('+xr.input_event.inputSources.length+'):'+xr.input_event.inputSources[0]._data)
+xr.input_event.inputSources.length && DEBUG_show(Date.now()+'('+xr.input_event.inputSources.length+'):'+xr.input_event.inputSources[0]._data)
 
   var hit_result = this.hit_test(frame)
 
