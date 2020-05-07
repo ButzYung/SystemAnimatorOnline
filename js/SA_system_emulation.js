60357603c6d2f9c1d8f2ac67ef08a5278a4a48da
ButzYung <butz@animetheme.com>
2020-05-07 16:20:27 +0800

    Update SA_system_emulation.js

--- js/SA_system_emulation.js
+++ js/SA_system_emulation.js
@@ -2928,13 +2928,13 @@ if (enabled == !!v)
 enabled = !!v
 
 if (enabled) {
-  MMD_SA._renderer.devicePixelRatio = window.devicePixelRatio/2
-  MMD_SA._renderer.__resize(EV_width, EV_height)
+//  MMD_SA._renderer.devicePixelRatio = 1
+//  MMD_SA._renderer.__resize(EV_width, EV_height)
   SL.style.visibility = "hidden"
 }
 else {
-  MMD_SA._renderer.devicePixelRatio = window.devicePixelRatio
-  MMD_SA._renderer.__resize(EV_width, EV_height)
+//  MMD_SA._renderer.devicePixelRatio = window.devicePixelRatio
+//  MMD_SA._renderer.__resize(EV_width, EV_height)
   SL.style.visibility = "visible"
   camera.video_canvas_bodyPix.style.visibility = "hidden"
 }
