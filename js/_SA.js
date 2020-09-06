608c6c9f82cd08676ce2e11288dc3867e5d7dec0
ButzYung <animethemegadget@gmail.com>
2020-09-06 23:45:53 +0800

    Fix a bug in Chrome fullscreen mode and WebXR AR mode due to window.resize issue

--- js/_SA.js
+++ js/_SA.js
@@ -2350,6 +2350,7 @@ System._browser._s_left = System._browser._s_top = null
   }
 
 // after all window moving/resizing timers (Electron v9+)
+// IMPORTANT: stick to webkit_electron_mode only, as this is probably not needed for browsers, and it causes issues in Chrome fullscreen mode and WebXR AR mode
   if (webkit_electron_mode)//webkit_mode)
     document.body.style._set()
 
