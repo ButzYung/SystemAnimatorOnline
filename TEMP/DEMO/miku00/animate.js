c9bbbe4e1e582a75911617e9424f8b686ee7feda
ButzYung <animethemegadget@gmail.com>
2020-10-17 19:30:45 +0800

    Update animate.js

--- TEMP/DEMO/miku00/animate.js
+++ TEMP/DEMO/miku00/animate.js
@@ -1393,7 +1393,7 @@ center.position.y -= Math.max(Math.min(model_speed.y/10, 2),-2)
 
 var rot
 
-rot = MMD_SA.TEMP_v3.set(Math.max(Math.min(model_speed.y/100, Math.PI/8),-Math.PI/8), Math.max(Math.min(model_speed.x/100, Math.PI/8),-Math.PI/8), 0);
+rot = MMD_SA.TEMP_v3.set(Math.max(Math.min(model_speed.y/100, Math.PI/8),-Math.PI/8), 0, Math.max(Math.min(model_speed.x/100, Math.PI/8),-Math.PI/8));
 if (orgy_level) {
   let time_diff = (RAF_timestamp - timestamp) / 1000
   orgy_spasm = (!orgy_spasm && (Math.random() < time_diff)) ? 2 : Math.max(orgy_spasm-time_diff*10, 0)
