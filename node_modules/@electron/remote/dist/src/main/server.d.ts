aa922447f19ce66f96c8481e2cc2fa39969734ca
ButzYung <animethemegadget@gmail.com>
2022-11-25 23:42:01 +0800

    Removed, to be packed in asar

--- node_modules/@electron/remote/dist/src/main/server.d.ts
+++
@@ -1,4 +0,0 @@
-import { WebContents } from 'electron';
-export declare const isRemoteModuleEnabled: (contents: WebContents) => boolean | undefined;
-export declare function enable(contents: WebContents): void;
-export declare function initialize(): void;
