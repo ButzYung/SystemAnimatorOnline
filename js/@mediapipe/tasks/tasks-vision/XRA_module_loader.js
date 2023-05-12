36ff7d6749a49284b178943ba0a41c1898927cae
ButzYung <animethemegadget@gmail.com>
2023-05-12 22:20:30 +0800

    Update XRA_module_loader.js

--- js/@mediapipe/tasks/tasks-vision/XRA_module_loader.js
+++ js/@mediapipe/tasks/tasks-vision/XRA_module_loader.js
@@ -1,5 +1,13 @@
-import('./vision_bundle.js').then((m)=>{
-//  console.log(m);
-  for (const name of ['FilesetResolver', 'FaceLandmarker', 'PoseLandmarker', 'HandLandmarker'])
-    self[name] = m[name];
-});
+// 2023-05-12
+
+if (typeof window !== "object") {
+  import('./vision_bundle.js').then((m)=>{
+//    console.log(m);
+    for (const name of ['FilesetResolver', 'FaceLandmarker', 'PoseLandmarker', 'HandLandmarker'])
+      self[name] = m[name];
+  });
+}
+else {
+  System._browser.load_script('js/@mediapipe/tasks/tasks-vision/vision_bundle.js');
+}
+
